3d58ee91531fe633cf674d0f3f4041115302e23b
OthmanSaud1 <Othman142183@gmail.com>
2022-06-17 17:56:44 +0300

    feat: proofOfWork

--- Block.js
+++ Block.js
@@ -1,18 +1,40 @@
 import { sha256 } from "crypto.js";
 class Block{
+    blockName = "Othman";
     constructor(hash, data, prev, hieght, nonce, timeStamp){
-        this.hash = this.calculate();
+        this.hash = this.proofOfWork();
         this.data = data;
         this.prev = prev;
         this.hieght = hieght;
         this.nonce = 0;
-        this.timeStamp = timeStamp;
+        this.timeStamp = Date.now();
 
     }
     calculate(){
        return sha256(
-           this.hieght + this.timeStamp + this.data + this.prev
+           this.hieght + this.timeStamp + this.data + this.prev + this.nonce
        ).toString();
     }
+    proofOfWork(){
+        let block = "OthCrypto";
+        let nonce = 0;
+        let numOfGuesses = 0;
+        let difficulty = 2;
+        
+        for (let i = 0; i < 10000000; i++){   
+        let magicHash = sha256(
+            block + nonce + Date.now()
+        ).toString();
+            console.log(magicHash);
+            if (magicHash.substring(0, difficulty) == "".padStart(difficulty, "0")){
+                console.log("magic hash : " + magicHash)
+                console.log("we found the hash with "+numOfGuesses+" guesses")
+                return
+            }
+        numOfGuesses++; 
+        nonce++;
+        }
+    }
 }
+
 export default Block;
--- BlockChain.js
+++ BlockChain.js
@@ -1,17 +1,24 @@
 import Block from "./Block.js";
-
+import { sha256 } from "crypto.js";
 class BlockChain{
-    chain;
+    chain
+    
     constructor(){
-        this.chain = [];
+        this.chain = []
         this.init();
+
     }
     init(){
         let block = new Block(0,"Genesis","null",0,0,Date.now());
-        if(this.chain.length() <= 0);{
+        if(this.chain.length == 0){
         this.chain.push(block);
         console.log(this.chain);
-        }
     }
+    
 }
+
+
+    
+}
+new BlockChain();
 export default BlockChain;
